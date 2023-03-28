1219f51977f3773da39c781b58e1697aec985f49
pieradellafrancesca <francesca.pierini3@gmail.com>
2023-03-28 23:46:56 +0200

    Esercizio del 28-03-2023

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,19 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import './src/styles/variables/_animations.scss';
+        @import './src/styles/variables/_colors.scss';
+        @import './src/styles/variables/_layout.scss';
+        @import './src/styles/variables/_typography.scss';
+        @import './src/styles/helpers/_mixins.scss';
+        @import './src/styles/helpers/_resolutions.scss';`,
+      },
+    },
+  },
   server: {
     host: true,
   },
